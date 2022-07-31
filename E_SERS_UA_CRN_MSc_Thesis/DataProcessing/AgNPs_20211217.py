620eb013809e50c62dec48714e7a7cbb4761326a
derek <derekzhou17@gmail.com>
2022-07-31 01:50:18 +0100

    Try 3D plot

--- E_SERS_UA_CRN_MSc_Thesis/DataProcessing/AgNPs_20211217.py
+++ E_SERS_UA_CRN_MSc_Thesis/DataProcessing/AgNPs_20211217.py
@@ -1,5 +1,7 @@
 from E_SERS_UA_CRN_MSc_Thesis.ReadData_Addition import *
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 
 path_gamry = '../E-SERS_UA+CRN+R6G_Addition/20211217/EC'
 path_raman = '../E-SERS_UA+CRN+R6G_Addition/20211217/SERS'
@@ -14,16 +16,30 @@ df_plot = df_mean[df_mean.Potential == -0.6].T
 df_plot.columns = df_plot.iloc[0].tolist()
 df_plot.drop(labels=['UA', 'CRN', 'R6G', 'Potential'], inplace=True)
 
-ax = plt.subplot()
-count = 0
-for i, col in df_plot.iteritems():
-    plt.plot(col+200*count)
-    ax.annotate(str(int(i)) + ' uM', (1700, 50+200*count))
-    count += 1
+fig = plt.figure(figsize=[9, 9])
+ax = fig.add_subplot(projection='3d')
+xs = list(df_plot.index)
 
-ax.set_title('UA + 0.1M NaF + 10uM R6G for AgNPs-APTES-FTO')
-ax.set_xlabel('Raman Shift ($\mathregular{cm^{-1}}$)')
-ax.set_ylabel('Intensity (counts $\mathregular{s^{-1}mW^{-1}}$)')
+for z in range(df_plot.shape[1]):
+    ys = df_plot.iloc[:, z].values
+    ax.plot(xs, ys, zs=z, zdir='y', alpha=0.8)
+    ax.scatter(xs[33], z, ys[33]+15, marker='*', color='black')
+    ax.scatter(xs[228], z, ys[228]+15, marker='x', color='black')
 
-plt.savefig('../Data_Fig/UA_CRN_0uM_20211217.png')
+ax.w_xaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
+ax.w_yaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
+#ax.w_zaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
+ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([1.1, 4.5, 0.5, 1]))
+ax.view_init(16, 278)
+
+# ax.set_xlabel('Raman Shift ($\mathregular{cm^{-1}}$)', fontsize=12, labelpad=6)
+# ax.set_ylabel('Uric acid Concentration ($\mu$M)', fontsize=12, labelpad=15)
+ax.set_yticks(range(df_plot.shape[1]))
+ax.set_yticklabels(list(df_plot))
+# ax.set_zlabel('Intensity (counts $\mathregular{s^{-1}mW^{-1}}$)', fontsize=12, labelpad=10)
+ax.set_zticks([100, 200])
+ax.set_zticklabels(['100', '200'])
+ax.grid(False)
+
+plt.savefig('../Data_Fig/UA_CRN_0uM_20211217_2.png')
 plt.show()
